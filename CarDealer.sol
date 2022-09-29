0c6448f580f465667aa2a0d7af49771887e567e0
cryptichic <syberchief@gmail.com>
2022-09-28 20:18:06 -0500

    added states and constructor

--- CarDealer.sol
+++ CarDealer.sol
@@ -4,9 +4,17 @@ pragma solidity ^0.8.16;
 contract CarDealer {
     
     // state variables
-    uint carPrice;
-    address public carBuyer;
-    address public carSeller;
+    address payable public carSeller;
+    address payable public carBuyer;
+    uint public carPrice;
 
-    
+    // state of the car status
+    enum CarStatus { Created, Locked, Release, Closed }
+    CarStatus public carStatus;
+
+    // constructor
+    constructor() payable {
+        carSeller = payable(msg.sender);
+        carPrice = msg.value;
+    }
 }
